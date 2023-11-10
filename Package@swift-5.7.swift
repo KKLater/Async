c7cd8f467a043861223b727d3e4f94f0c0f28273
luoshuxin <luoshuxin@58.com>
2023-11-10 16:48:15 +0800

    MOD： add support Package

--- Package@swift-5.7.swift
+++ Package@swift-5.7.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,10 +14,6 @@ let package = Package(
     products: [
         .library(
             name: "Async",
-            targets: ["Async"]),
-        .library(
-            name: "AsyncKit",
-            type: .static,
             targets: ["Async"]),
     ],
     targets: [
